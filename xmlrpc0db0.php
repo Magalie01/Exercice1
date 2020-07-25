3f9e413a01cfbe2515c823b163e792b79dfc088f
Magalie01 <63805980+Magalie01@users.noreply.github.com>
2020-07-25 16:58:32 +0100

    Add files via upload

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -1,14 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
 	<service>
-		<engineName>WordPress</engineName>
-		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>http://dbsgabon.com</homePageLink>
+		<engineName>GitHub</engineName>
+		<engineLink>https://github.com/</engineLink>
+		<homePageLink>https://magalie01.github.io/Exercice1/</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="http://dbsgabon.com/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="http://dbsgabon.com/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://dbsgabon.com/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="http://dbsgabon.com/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="http://dbsgabon.com/wp-json/" />
+		
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://magalie01.github.io/Exercice1/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://magalie01.github.io/Exercice1/xmlrpc.php"/>
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://magalie01.github.io/Exercice1/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://magalie01.github.io/Exercice1/xmlrpc.php" />
 			</apis>
 	</service>
 </rsd>
